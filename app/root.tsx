161a2153f4dbef9f977c3c45e4b29643924159bc
Thebarda <tomdar87@outlook.com>
2022-09-10 17:42:22 +0200

    Try to repload images

--- app/root.tsx
+++ app/root.tsx
@@ -9,12 +9,20 @@ import {
 } from "@remix-run/react";
 import Header from "./components/Header";
 import ThemeProvider from "./components/ThemeProvider";
+import guess from './assets/guess.png';
+import personal from './assets/personal.png';
+import thebardabot from './assets/thebardabot.png';
+import type { LinksFunction } from "@remix-run/react/dist/routeModules";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Tom Darneix Personal Website",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => {
+  return [{ rel: "preload", href: guess, as: 'image' }, { rel: "preload", href: personal, as: 'image' }, { rel: "preload", href: thebardabot, as: 'image' }];
+};
 
 export default function App() {
   return (
