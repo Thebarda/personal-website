713484622ad67ca4c62c40420c93424cb64bc7e0
Thebarda <tomdar87@outlook.com>
2022-09-23 18:44:33 +0200

    Fix theme

--- app/components/ThemeProvider/atoms.tsx
+++
@@ -1,4 +0,0 @@
-import { atom } from 'jotai';
-import { ThemeMode } from './models';
-
-export const themeAtom = atom(ThemeMode.System);
--- app/components/ThemeProvider/useGetTheme.ts
+++ app/components/ThemeProvider/useGetTheme.ts
@@ -1,35 +1,34 @@
-import { useEffect, useState, useCallback } from 'react';
-import { useAtomValue } from 'jotai';
-import { equals } from 'ramda';
+import { useLayoutEffect, useState, useCallback } from "react";
+import { equals } from "ramda";
 
-import { themeAtom } from "./atoms";
-import { ThemeMode } from './models';
+import { ThemeMode } from "./models";
 
 export const isDarkTheme = equals<ThemeMode | null>(ThemeMode.Dark);
 export const isSystemTheme = equals(ThemeMode.System);
 
-const getSystemTheme = (matches: boolean): ThemeMode => matches ? ThemeMode.Dark : ThemeMode.Light;
+const getSystemTheme = (matches: boolean): ThemeMode =>
+  matches ? ThemeMode.Dark : ThemeMode.Light;
 
 export const useGetTheme = (): ThemeMode | null => {
-  const theme = useAtomValue(themeAtom);
-  const [muiTheme, setMuiTheme] = useState(isSystemTheme(theme) ? null : theme)
-  
+  const [muiTheme, setMuiTheme] = useState(
+    getSystemTheme(window.matchMedia("(prefers-color-scheme: dark)").matches)
+  );
+
   const changeSystemTheme = useCallback((matches: boolean) => {
     setMuiTheme(getSystemTheme(matches));
   }, []);
-  
-  useEffect(() => {
-    if (!isSystemTheme(theme)) {
-      setMuiTheme(theme);
-      return;
-    }
-    
-    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-    changeSystemTheme(darkThemeMq.matches);
-    darkThemeMq.addEventListener('change', (event) => changeSystemTheme(event.matches));
 
-    return () => darkThemeMq.removeEventListener('change', (event) => changeSystemTheme(event.matches));
-  }, [theme, changeSystemTheme]);
+  useLayoutEffect(() => {
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    darkThemeMq.addEventListener("change", (event) =>
+      changeSystemTheme(event.matches)
+    );
+
+    return () =>
+      darkThemeMq.removeEventListener("change", (event) =>
+        changeSystemTheme(event.matches)
+      );
+  }, []);
 
   return muiTheme;
-}
+};
